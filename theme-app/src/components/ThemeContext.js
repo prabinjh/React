cba954b68785495468bba3bd65439e361011f3a7
prabinjh <jhprabin555@gmail.com>
2025-03-08 11:56:36 +0530

    Final changes before merging dev-1 to main

--- theme-app/src/components/ThemeContext.js
+++ theme-app/src/components/ThemeContext.js
@@ -1,19 +1,18 @@
 import React, { createContext, useState, useContext } from "react";
- 
+
 const ThemeContext = createContext();
 
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+               const [theme, setTheme] = useState("light");
+               const toggleTheme = () => {
+                              setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+                              };
 
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
-  };
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      <div className={theme}>{children}</div>
-    </ThemeContext.Provider>
-  );
+return (
+<ThemeContext.Provider value={{ theme, toggleTheme }}>
+<div className={theme}>{children}</div>
+</ThemeContext.Provider>
+);
 };
